a6d7c41be12150971fe3dc40da0404c2a6b636f7
Jesse Kelly <kellyjesse881@gmail.com>
2021-01-19 12:06:19 -0600

    feat: added minimap and ligatures

--- config.el
+++ config.el
@@ -7,20 +7,27 @@
 
 (setq doom-font (font-spec :family "Hack" :size 15)
       doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
-      doom-big-font (font-spec :family "Hack" :size 16))
+      doom-big-font (font-spec :family "Hack" :size 20))
 
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 
+(custom-set-faces!
+  '(font-lock-comment-face :slant italic)
+  '(font-lock-keyword-face :slant italic))
+
+(after! org
+  (setq org-directory "~/org/"
+        org-hide-emphasis-markers t))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'doom-spacegrey)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
